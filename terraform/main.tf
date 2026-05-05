f83353a63abc712812c41adc1f66762e7d518b44
Naveen Gadde <gaddenaveen888@gmail.com>
2026-05-05 16:22:02 +0530

    modified deploy.yaml and main.tf files

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,17 @@ provider "azurerm" {
   features {}
 }
 
+# -------------------------
+# Resource Group
+# -------------------------
 resource "azurerm_resource_group" "rg" {
   name     = "rg-gitlab"
   location = "Central US"
 }
 
+# -------------------------
+# Virtual Network
+# -------------------------
 resource "azurerm_virtual_network" "vnet" {
   name                = "aks-vnet"
   address_space       = ["10.0.0.0/8"]
@@ -14,6 +20,9 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+# -------------------------
+# Subnets
+# -------------------------
 resource "azurerm_subnet" "aks_subnet" {
   name                 = "aks-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -28,6 +37,9 @@ resource "azurerm_subnet" "appgw_subnet" {
   address_prefixes     = ["10.241.0.0/16"]
 }
 
+# -------------------------
+# Public IP for App Gateway
+# -------------------------
 resource "azurerm_public_ip" "appgw_pip" {
   name                = "appgw-pip"
   location            = azurerm_resource_group.rg.location
@@ -36,6 +48,9 @@ resource "azurerm_public_ip" "appgw_pip" {
   sku                 = "Standard"
 }
 
+# -------------------------
+# Application Gateway (FIXED)
+# -------------------------
 resource "azurerm_application_gateway" "appgw" {
   name                = "gitlab-appgw"
   location            = azurerm_resource_group.rg.location
@@ -61,8 +76,39 @@ resource "azurerm_application_gateway" "appgw" {
     name = "http-port"
     port = 80
   }
+
+  backend_address_pool {
+    name = "dummy-backend"
+  }
+
+  backend_http_settings {
+    name                  = "http-setting"
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 30
+  }
+
+  http_listener {
+    name                           = "listener"
+    frontend_ip_configuration_name = "frontend-ip"
+    frontend_port_name             = "http-port"
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = "rule1"
+    rule_type                  = "Basic"
+    http_listener_name         = "listener"
+    backend_address_pool_name  = "dummy-backend"
+    backend_http_settings_name = "http-setting"
+    priority                   = 1
+  }
 }
 
+# -------------------------
+# AKS Cluster with AGIC
+# -------------------------
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-gitlab-eip"
   location            = azurerm_resource_group.rg.location
@@ -70,10 +116,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = "gitlabaks"
 
   default_node_pool {
-    name            = "agentpool"
-    node_count      = 2
-    vm_size         = "Standard_B2ps_v2"
-    vnet_subnet_id  = azurerm_subnet.aks_subnet.id
+    name           = "agentpool"
+    node_count     = 2
+    vm_size        = "Standard_B2ps_v2"
+    vnet_subnet_id = azurerm_subnet.aks_subnet.id
   }
 
   identity {
@@ -87,4 +133,43 @@ resource "azurerm_kubernetes_cluster" "aks" {
   ingress_application_gateway {
     gateway_id = azurerm_application_gateway.appgw.id
   }
+}
+
+# -------------------------
+# PostgreSQL (ADDED)
+# -------------------------
+resource "azurerm_postgresql_flexible_server" "pg" {
+  name                   = "gitlab-postgres"
+  resource_group_name    = azurerm_resource_group.rg.name
+  location               = azurerm_resource_group.rg.location
+  version                = "13"
+
+  administrator_login    = "gitlabadmin"
+  administrator_password = "StrongPass@123"
+
+  storage_mb = 32768
+  sku_name   = "B_Standard_B1ms"
+}
+
+# -------------------------
+# Redis (ADDED)
+# -------------------------
+resource "azurerm_redis_cache" "redis" {
+  name                = "gitlab-redis"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  capacity            = 0
+  family              = "C"
+  sku_name            = "Basic"
+}
+
+# -------------------------
+# Storage Account (ADDED)
+# -------------------------
+resource "azurerm_storage_account" "storage" {
+  name                     = "gitlabstorage12345"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
