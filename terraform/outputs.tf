fdaad598466af5592372a1579f2ba6effe2b78fc
Naveen Gadde <gaddenaveen888@gmail.com>
2026-05-06 09:38:36 +0530

    modified deploy and outputs.yaml files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,21 +1,26 @@
 output "postgres_host" {
-  value = azurerm_postgresql_flexible_server.pg.fqdn
+  value       = azurerm_postgresql_flexible_server.pg.fqdn
+  description = "PostgreSQL Flexible Server FQDN"
 }
 
 output "redis_host" {
-  value = azurerm_redis_cache.redis.hostname
+  value       = azurerm_redis_cache.redis.hostname
+  description = "Redis hostname"
 }
 
 output "redis_key" {
-  value     = azurerm_redis_cache.redis.primary_access_key
-  sensitive = true
+  value       = azurerm_redis_cache.redis.primary_access_key
+  sensitive   = true
+  description = "Redis primary access key"
 }
 
 output "storage_account" {
-  value = azurerm_storage_account.storage.name
+  value       = azurerm_storage_account.storage.name
+  description = "Storage account name"
 }
 
 output "storage_key" {
-  value     = azurerm_storage_account.storage.primary_access_key
-  sensitive = true
+  value       = azurerm_storage_account.storage.primary_access_key
+  sensitive   = true
+  description = "Storage account key"
 }
